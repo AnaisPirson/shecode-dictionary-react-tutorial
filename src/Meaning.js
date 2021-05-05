b6e35a746ca12bf4954c53ddc4061a0dc58b6d62
AnaisPirson <anaispirson@ymail.com>
2021-05-05 23:28:41 +0200

    Created Synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,25 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech} </h3>
+      <h3>
+        <strong>{props.meaning.partOfSpeech} </strong>
+      </h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
-            <p> {definition.example}</p>
+            <p>
+              <strong>Definition:</strong> {definition.definition}
+              <br />
+              <strong>Sentence: </strong> {definition.example}
+              <br />
+              <em>
+                <Synonyms synonyms={definition.synonyms} />
+              </em>
+            </p>
           </div>
         );
       })}
